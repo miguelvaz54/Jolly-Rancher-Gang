adf7c861d12b1ca7c9a04a5370585a4ab2576104
miguelvaz54 <miguelvaz54@gmail.com>
2024-09-08 22:58:16 +0000

    testing

---
+++ Assignment1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char **argv) {
+    //The main function encapsulates and calls the other functions; returns 0
+    calculateDistance();
+    calculatePerimeter();
+    calculateArea():
+    calculateWidth();
+    calculateHeight();
+}
